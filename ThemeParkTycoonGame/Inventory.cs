eedb8b93f06f64d54bc4457499e3c1e64177d724
Timothy Lutt <thejjokerr@gmail.com>
2019-10-21 17:21:55 +0200

    Prepare for inventory system

---
+++ ThemeParkTycoonGame/Inventory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThemeParkTycoonGame
+{
+    public class Inventory
+    {
+        public List<Shop> PurchasedShops;
+        public List<Ride> PurchasedRides;
+    }
+}
